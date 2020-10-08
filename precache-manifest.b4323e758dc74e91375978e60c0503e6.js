aed76dced564b2484fb76eea6d0e2f8c12806499
artunn <curaartun@gmail.com>
2020-10-08 17:34:53 +0300

    Updates

---
+++ precache-manifest.b4323e758dc74e91375978e60c0503e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8914838ba444d83b772d12d56b0378c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f883dd131c78f3f605c4",
+    "url": "/static/css/main.87f5878e.chunk.css"
+  },
+  {
+    "revision": "28b17140c310bc5f806b",
+    "url": "/static/js/2.fcb751fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fcb751fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f883dd131c78f3f605c4",
+    "url": "/static/js/main.7a8f39f3.chunk.js"
+  },
+  {
+    "revision": "7b5bc4727ec970bc673a",
+    "url": "/static/js/runtime-main.d046d4b5.js"
+  }
+]);
